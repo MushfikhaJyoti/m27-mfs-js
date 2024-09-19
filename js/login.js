c4436439542e1be6c4920c0f73a7576dd08b50be
Mushfikha <mushfikha0894@gmail.com>
2024-09-19 06:53:35 +0600

    made the login button acceptable and move to another page

--- js/login.js
+++ js/login.js
@@ -4,7 +4,18 @@ document.getElementById('login-btn').addEventListener("click", function(event){
     // step 2 = stop reloading brouser for "form" tag;
     event.preventDefault();
     console.log('button clicked');
-    // step 3 = get the phone number
+    // step 3 = get the phone number and pin number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-num').value;
+    console.log(phoneNumber, pinNumber);
+
+    // A TEMPORARY AND NOT RECOMENDED WAY TO CHECK PHONE AND PIN;
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('You are logged in');
+        window.location.href = '/homepage.html';
+    } 
+    // step 5 = allow user to use the website;
+    else{
+        alert('wrong phone number or pin');
+    }
 });
