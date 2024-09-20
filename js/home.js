8be8713ed3d8bdadccef1a22b29aac27c821f114
Mushfikha <mushfikha0894@gmail.com>
2024-09-20 09:54:59 +0600

    home page input fields done

--- js/home.js
+++ js/home.js
@@ -1,1 +1,17 @@
-console.log("home added");
+// add money to the acc;
+
+// Step-1 = add event handler && prevent page reload after from submit;
+
+
+document.getElementById('btn-add-money')
+ .addEventListener('click', function(event){
+    event.preventDefault();
+   
+
+// Step-2 = get money to be added to the acc balance ;
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+    // get the pin number provided;
+    const inputPinNumber = document.getElementById('input-pin-number').value;
+    console.log(inputPinNumber);
+});
